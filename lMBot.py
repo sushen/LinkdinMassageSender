510827bf9ea3fed3f6042b267e41b350f19631b4
sushen <sushenbiswasaga@gmail.com>
2020-08-11 15:33:53 +0600

    Start Making

---
+++ lMBot.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+
+
+class LinkdinMassageBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome("C:\\seleniumDriver\\chromedriver.exe")
+        # driver.get("https://sushenbiswas.com")
