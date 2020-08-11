60f2ea27a3b39e855eb2462cff23ec264db1ac7b
Sushen Biswas <sushenbiswasaga@gmail.com>
2020-08-11 22:46:34 +0600

    Commit

--- lMBot.py
+++ lMBot.py
@@ -1,8 +1,16 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager( ).install())
+driver = webdriver.Chrome("C:\\seleniumDriver\\chromedriver.exe")
+driver.get("https://sushenbiswas.com")
+driver.maximize_window()
+driver.implicitly_wait(20)
+driver.quit()
 
 
-
-class LinkdinMassageBot():
-    def __init__(self):
-        self.driver = webdriver.Chrome("C:\\seleniumDriver\\chromedriver.exe")
-        # driver.get("https://sushenbiswas.com")
+# class LinkdinMassageBot():
+#     def __init__(self):
+#         driver = webdriver.Chrome(ChromeDriverManager().install())
+#         self.driver = webdriver.Chrome("C:\\seleniumDriver\\chromedriver.exe")
+#         driver.get("https://sushenbiswas.com")
